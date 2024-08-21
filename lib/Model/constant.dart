a0c6d45e826471797b2af750733b7c767d0151cc
mogalia <mogalia786@gmail.com>
2024-08-21 12:58:58 +0200

    Initial commit

---
+++ lib/Model/constant.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const String googleApiKey = 'AIzaSyAcqpzOvxzn1GHebVAuxZC-25EmCr3n1bs';
+const String publicKey = 'pk_test_cb844d85128c15f00f27d167ddb59693bfe5ee4e';
+dynamic appColor = Colors.orange;
+const String facebookLink = '';
+const String twitterLink = '';
+const String instagram = '';
+const String email = 'mogalia786@gmail.com';
+const String serverUrl = 'http//www.mogalia.co.za';
